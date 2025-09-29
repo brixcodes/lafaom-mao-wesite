cf16d5450e00e4716c8b41677f6a60d075d8825e
brixcodes <nanyangbrice.devops@gmail.com>
2025-09-29 17:58:08 +0100

    ajustements

--- src/app/pages/formations/formations.ts
+++ src/app/pages/formations/formations.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Header } from '../../components/formations/header/header';
+import { Filters } from '../../components/formations/filters/filters';
 
 @Component({
   selector: 'app-formations',
-  imports: [Header],
+  imports: [Header, Filters],
   templateUrl: './formations.html',
   styleUrl: './formations.css'
 })
